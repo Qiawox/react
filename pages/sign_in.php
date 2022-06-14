c13675c5a71b357841eefcca6f382dcbdf1a43eb
Qiawox <gv7361299@gmail.com>
2022-06-14 17:40:57 +0400

    Update sign_in.php

--- pages/sign_in.php
+++ pages/sign_in.php
@@ -26,15 +26,11 @@
   crossorigin="anonymous">
 </script>
 <script>
-$('button').on('click', function(){
-  const email = $('input.email').val();
-  localStorage.setItem('email', email);
-  const password = $('input.password').val();
-  localStorage.setItem('password', password);
-});
 function Auto() {
   const email = $('input.email').val();
   const password = $('input.password').val();
+  localStorage.setItem('email', email);
+  localStorage.setItem('password', password);
   const fromStorage = localStorage.getItem('email');
   const fromStorage1 = localStorage.getItem('password');
   if (fromStorage || fromStorage1) {
